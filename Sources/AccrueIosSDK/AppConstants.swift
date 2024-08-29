4f2fa7b8096788eb1924e4a417945993f68e1d09
othecos <othecos@gmail.com>
2024-08-29 10:55:46 -0300

    chore: wrong tls

--- Sources/AccrueIosSDK/AppConstants.swift
+++ Sources/AccrueIosSDK/AppConstants.swift
@@ -1,6 +1,6 @@
 public struct AppConstants {
     // Mocked testing environment
-    static let apiBaseUrl:String = "https://localhost:5173/offer"
+    static let apiBaseUrl:String = "http://localhost:5173/offer"
     // Mocked merchantId
     static let merchantId: String = "08b13e48-06be-488f-9a93-91f59d94f30d"
     static let redirectionToken = "redirection-token"
