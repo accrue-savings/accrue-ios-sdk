205f982b29158c4a2bdf83fa8f20308d07bd9d25
othecos <othecos@gmail.com>
2025-02-10 12:20:33 -0300

    chore: try to set max zoom

--- Sources/AccrueIosSDK/AccrueWebView.swift
+++ Sources/AccrueIosSDK/AccrueWebView.swift
@@ -86,9 +86,8 @@ public struct AccrueWebView: UIViewRepresentable {
         // Set the navigation delegate
         webView.navigationDelegate = context.coordinator
         webView.uiDelegate = context.coordinator
-        webView.isMultipleTouchEnabled = false
-        webView.scrollView.pinchGestureRecognizer?.isEnabled = false
-
+        webView.scrollView.minimumZoomScale = 1.0
+        webView.scrollView.maximumZoomScale = 1.0
         if #available(iOS 16.4, *) {
             webView.isInspectable = true // Safe to use isInspectable here
         }
