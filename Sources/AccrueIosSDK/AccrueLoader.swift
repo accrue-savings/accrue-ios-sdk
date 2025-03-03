6099cfe6f0eecdde6e9caa4f41fe8409cba1c9d1
othecos <othecos@gmail.com>
2025-03-03 17:48:19 -0500

    chore: debug 19

--- Sources/AccrueIosSDK/AccrueLoader.swift
+++ Sources/AccrueIosSDK/AccrueLoader.swift
@@ -4,21 +4,21 @@ struct AccrueLoader: View {
     @State var loading = false
     
     var body: some View {
-        HStack(spacing: 1) {
+        HStack(spacing: 4) {
             Circle()
                 .fill(Color.gray)
                 .frame(width: 10, height: 10)
-                .scaleEffect(loading ? 1.5 : 0.5)
+                .scaleEffect(loading ? 1 : 0.5)
                 .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true), value: loading)
             Circle()
                 .fill(Color.gray)
                 .frame(width: 10, height: 10)
-                .scaleEffect(loading ? 1.5 : 0.5)
+                .scaleEffect(loading ? 1 : 0.5)
                 .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true).delay(0.2), value: loading)
             Circle()
                 .fill(Color.gray)
                 .frame(width: 10, height: 10)
-                .scaleEffect(loading ? 1.5 : 0.5)
+                .scaleEffect(loading ? 1 : 0.5)
                 .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true).delay(0.4), value: loading)
         }
         .onAppear() {
