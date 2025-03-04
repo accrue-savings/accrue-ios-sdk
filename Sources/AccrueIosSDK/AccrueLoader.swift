31fedc1e37ddb06859e01a6005e3c3d2b43674a4
othecos <othecos@gmail.com>
2025-03-04 11:07:17 -0300

    feat: reload webview when on foreground

---
+++ Sources/AccrueIosSDK/AccrueLoader.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct AccrueLoader: View {
+    @State var loading = false
+    
+    var body: some View {
+        HStack(spacing: 5) {
+            Circle()
+                .fill(Color.gray)
+                .frame(width: 10, height: 10)
+                .scaleEffect(loading ? 1.4 : 1)
+                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true), value: loading)
+            Circle()
+                .fill(Color.gray)
+                .frame(width: 10, height: 10)
+                .scaleEffect(loading ? 1.4 : 1)
+                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true).delay(0.2), value: loading)
+            Circle()
+                .fill(Color.gray)
+                .frame(width: 10, height: 10)
+                .scaleEffect(loading ? 1.4 : 1)
+                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true).delay(0.4), value: loading)
+        }
+        .onAppear() {
+            self.loading = true
+        }
+    }
+}
