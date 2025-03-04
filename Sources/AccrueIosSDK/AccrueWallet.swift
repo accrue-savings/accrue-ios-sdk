b92fced35976bdfe02d507efda1fda630f5ce203
othecos <othecos@gmail.com>
2025-03-04 11:07:17 -0300

    Add loader to webview (#10)
    * feat: add loader to webview
    
    * fix: swift ui fixes
    
    * fix: swift ui fixes 2
    
    * fix: swift ui fixes 3
    
    * fix: swift ui fixes 4
    
    * fix: swift ui fixes 5
    
    * fix: swift ui fixes 6
    
    * chore: debug
    
    * chore: debug 2
    
    * chore: debug 3
    
    * chore: debug 4
    
    * chore: debug 5
    
    * chore: debug 6
    
    * chore: debug 7
    
    * chore: debug 8
    
    * chore: debug 9
    
    * chore: debug 10
    
    * chore: debug 11
    
    * chore: debug 12
    
    * chore: debug 13
    
    * chore: debug 13
    
    * chore: debug 14
    
    * chore: debug 15
    
    * chore: debug 16
    
    * chore: debug 17
    
    * chore: debug 18
    
    * chore: debug 19
    
    * chore: debug 20
    
    * chore: debug 21
    
    * chore: debug 22
    
    * chore: debug 23
    
    * chore: debug 24
    
    * fix: remove console log
    
    * fix: add optional loader
    
    * fix: missing prop
    
    * fix: remove loader

--- Sources/AccrueIosSDK/AccrueWallet.swift
+++ Sources/AccrueIosSDK/AccrueWallet.swift
@@ -7,29 +7,42 @@ public struct AccrueWallet: View {
     public let isSandbox: Bool
     public let url: String?
     public var onAction: ((String) -> Void)?
-    
+    public var shouldShowLoader: Bool = false
+    @State private var isLoading: Bool = false
+
     @ObservedObject var contextData: AccrueContextData
 #if os(iOS)
     private var WebViewComponent: AccrueWebView {
         let fallbackUrl = URL(string: AppConstants.productionUrl)!
         let url = buildURL(isSandbox: isSandbox, url: url) ?? fallbackUrl
         
-        return AccrueWebView(url: url, contextData: contextData, onAction: onAction)
+        return AccrueWebView(url: url, contextData: contextData, onAction: onAction, isLoading: $isLoading)
     }
 #endif
  
-    public init(merchantId: String, redirectionToken: String?,isSandbox: Bool,url: String? = nil, contextData: AccrueContextData = AccrueContextData(), onAction: ((String) -> Void)? = nil) {
+    public init(merchantId: String, redirectionToken: String?,isSandbox: Bool,url: String? = nil, contextData: AccrueContextData = AccrueContextData(), onAction: ((String) -> Void)? = nil, shouldShowLoader: Bool = false) {
         self.merchantId = merchantId
         self.redirectionToken = redirectionToken
         self.contextData = contextData
         self.isSandbox = isSandbox
         self.url = url
         self.onAction = onAction
+        self.shouldShowLoader = shouldShowLoader
     }
     
     public var body: some View {
 #if os(iOS)
+    ZStack {
         WebViewComponent
+        if isLoading && shouldShowLoader {
+            VStack {
+                AccrueLoader()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white.opacity(0.8))
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
 #endif
     }
     
